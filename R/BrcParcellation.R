0d5d3037694cd75f1bd82cb38b61f0e9c385025b
Daniel Greenidge <cdgreenidge@users.noreply.github.com>
2016-06-29 14:38:12 -0400

    Remove default parcellation from BrcParcellation
    Design flaw: constructor does too much work

--- R/BrcParcellation.R
+++ R/BrcParcellation.R
@@ -1,4 +1,4 @@
-BrcParcellation <- function(dim3d, partition=NULL) {
+BrcParcellation <- function(dim3d, partition) {
   if (!is.numeric(dim3d)) {
     stop("dim3d argument must be a 3-element numeric vector")
   } else if (length(dim3d) != 3) {
@@ -6,11 +6,6 @@ BrcParcellation <- function(dim3d, partition=NULL) {
   }
 
   NUM_3D_VOXELS <- Reduce("*", dim3d)
-
-  if (is.null(partition)) {
-    partition <- factor(1:NUM_3D_VOXELS)
-  }
-
   if (length(partition) != NUM_3D_VOXELS) {
     stop("partition factor length not equal to number of 3D voxels")
   }
--- tests/testthat/test_BrcParcellation.R
+++ tests/testthat/test_BrcParcellation.R
@@ -1,12 +1,12 @@
 context("Test BrcParcellation.R")
 
 test_that("the constructor returns an object of class BrcParcellation", {
-  parcellation <- BrcParcellation(dim3d=c(2, 2, 2))
+  parcellation <- BrcParcellation(dim3d=c(2, 2, 2), factor(1:8))
   expect_equal(class(parcellation), "BrcParcellation")
 })
 
 test_that("it contains a 3-element dimension vector", {
-  parcellation <- BrcParcellation(dim3d=c(2, 2, 2))
+  parcellation <- BrcParcellation(dim3d=c(2, 2, 2), factor(1:8))
   expect_equal(dim3d(parcellation), c(2, 2, 2))
 })
 
@@ -22,11 +22,6 @@ test_that("it contains a partition factor", {
   part <- factor(1:8)
   parcellation <- BrcParcellation(dim3d=c(2, 2, 2), partition=part)
   expect_equal(partition(parcellation), part)
-})
-
-test_that("the default partition puts every voxel in its own parcel", {
-  parcellation <- BrcParcellation(dim3d=c(2, 2, 2))
-  expect_equal(partition(parcellation), factor(c(1, 2, 3, 4, 5, 6, 7, 8)))
 })
 
 test_that("the partition must contain an entry for each 3D voxel", {
