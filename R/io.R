fecf7281cbb895b6fd7cd882cbde57eb1985bdef
Daniel Greenidge <cdgreenidge@users.noreply.github.com>
2016-07-06 13:59:33 -0400

    Add data2dTo4d function

--- R/io.R
+++ R/io.R
@@ -8,8 +8,30 @@ niftiToBrcFmri <- function(file, id=NULL) {
   buildBrcFmri(data=mat2d, dim3d=dim(nifti)[-4], id=id, partition=NULL)
 }
 
+data2dTo4d <- function(mat2d, parcellation) {
+  part <- partition(parcellation)
+  expandedMatrix <- .expandMatrix(mat2d, part)
+  array(data=t(expandedMatrix), dim=c(dim3d(parcellation), nrow(mat2d)))
+}
+
 data4dTo2d <- function(arr4d) {
   num3dVoxels <- Reduce("*", dim(arr4d)[-4])
   scanLength <- dim(arr4d)[4]
   t(matrix(data=arr4d, ncol=scanLength, nrow=num3dVoxels))
+}
+
+.expandMatrix <- function(mat2d, partition) {
+  partition <- .factorToNumeric(partition)
+
+  compressedIndices <- partition[partition != 0]
+  expandedIndices <- (1:length(partition))[partition != 0]
+
+  expandedMatrix <- matrix(data=0, nrow=nrow(mat2d), ncol=length(partition))
+  expandedMatrix[ , expandedIndices] <- mat2d[ , compressedIndices]
+
+  expandedMatrix
+}
+
+.factorToNumeric <- function(f) {
+  as.numeric(levels(f))[f]
 }
