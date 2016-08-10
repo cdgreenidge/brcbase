52a66149489356d37c5a9dad6276ea9cc38aeb46
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-08-10 15:49:31 -0400

    Allowed reduce_mean and reduce_pca to handle empty matrices

--- R/reduce.BrcFmri.R
+++ R/reduce.BrcFmri.R
@@ -6,11 +6,16 @@ reduce.BrcFmri <- function(x, y, func = reduce_mean){
 reduce_mean <- function(mat){
   stopifnot(is.matrix(mat))
   
+  if(ncol(mat) == 0) return(rep(0, nrow(mat)))
+  
   apply(mat, 1, mean)
 }
 
 reduce_pca <- function(mat){
   stopifnot(is.matrix(mat))
   
+  if(ncol(mat) == 0) return(rep(0, nrow(mat)))
+  
   stats::prcomp(mat, center = T, scale. = T)$x[,1]
 }
+
--- tests/testthat/test_reduce.BrcFmri.R
+++ tests/testthat/test_reduce.BrcFmri.R
@@ -10,6 +10,19 @@ test_that("it computes the correct mean", {
   expect_true(length(res) == 20)
 })
 
+test_that("it errors when passed in nothing", {
+  expect_error(reduce_mean(NULL))
+  expect_error(reduce_mean(NA))
+  expect_error(reduce_mean())
+})
+
+test_that("it can handle matrix with no columns", {
+  mat <- matrix(0, 10, 10)
+  res <- reduce_mean(mat[,numeric(0)])
+  expect_true(length(res) == 10)
+  expect_true(all(res == 0))
+})
+
 ## test reduce_pca()
 
 test_that("it computes the right pca", {
@@ -24,3 +37,21 @@ test_that("it computes the right pca", {
   expect_true(all(order(x) == order(res)) | 
                 all(order(x) == rev(order(res))))
 })
+
+test_that("it errors when passed in nothing", {
+  expect_error(reduce_pca(NULL))
+  expect_error(reduce_pca(NA))
+  expect_error(reduce_pca())
+})
+
+test_that("it can handle matrix with no columns", {
+  mat <- matrix(0, 10, 10)
+  res <- reduce_pca(mat[,numeric(0)])
+  expect_true(length(res) == 10)
+  expect_true(all(res == 0))
+})
+
+###########
+
+## test .isValid_reduction()
+
