e8fb902463cad54b13e085deee329dd6b6606cc7
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-08-23 21:48:23 -0400

    Modified checkMotion so it can handle 0's in the middle of the time series

--- R/checkMotionIntersect.R
+++ R/checkMotionIntersect.R
@@ -1,17 +1,19 @@
 checkMotion.array <- function(obj){
- dim.vec <- dim(obj)
- stopifnot(length(dim.vec) >= 3, length(dim.vec) <= 4)
- if(length(dim.vec) == 3) return(TRUE)
- 
- idx <- which(obj[,,,1] != 0)
- data.slice <- apply(obj, c(1,2,3), function(x){sum(abs(x))})
- idx2 <- which(data.slice != 0)
- 
- if(length(idx) != length(idx2)) {
+  dim.vec <- dim(obj)
+  stopifnot(length(dim.vec) >= 3, length(dim.vec) <= 4)
+  if(length(dim.vec) == 3) return(TRUE)
+  
+  idx <- which(obj[,,,1] != 0)
+  data.slice <- apply(obj, c(1,2,3), function(x){
+    if(any(x == 0)) 0 else 1
+  })
+  idx2 <- which(data.slice != 0)
+  
+  if(length(idx) != length(idx2)) {
     return(FALSE)
- } else {
+  } else {
    return(TRUE)
- }
+  }
 }
 
 checkMotion.nifti <- function(obj){
--- tests/testthat/test_checkMotionIntersect.R
+++ tests/testthat/test_checkMotionIntersect.R
@@ -8,14 +8,28 @@ test_that("it errors if not dimension 3 or 4", {
 })
 
 test_that("it returns false if there is motion", {
-  mat <- array(0, rep(3,4))
+  mat <- array(0, rep(2,4))
   mat[1,1,1,1] <- 1
-  mat[1,1,2,3] <- 1
+  mat[1,1,2,2] <- 1
   expect_true(!checkMotion(mat))
 })
 
 test_that("it returns true for dimension 3", {
   mat <- array(0, rep(3,3))
+  expect_true(checkMotion(mat))
+})
+
+test_that("it returns false if there are 0 in the middle of the time series",{
+  mat <- array(0, c(2,2,2,3))
+  mat[1,1,1,1] <- 1
+  expect_true(!checkMotion(mat))
+})
+
+test_that("it returns true normally", {
+  mat <- array(0, rep(3,4))
+  mat[1,1,1,] <- 1
+  mat[1,3,2,] <- rnorm(3)
+  mat[2,2,2,] <- -3
   expect_true(checkMotion(mat))
 })
 
