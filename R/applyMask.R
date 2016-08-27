19a820eac81a7fe207bfac29920ca923ac7253f5
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-08-27 17:48:33 -0400

    Added documentation for applyMask

--- R/applyMask.R
+++ R/applyMask.R
@@ -1,3 +1,29 @@
+#' Applying a Mask (\code{BrcParcellation}) To \code{BrcFmri}
+#' 
+#' Given a \code{BrcFmri} instance (\code{fmri}) and \code{BrcParcellation}
+#' instance (\code{parcellation}), \code{applyMask} intersects \code{fmri}
+#' by \code{parcellation}. This means all the voxels in \code{parcellation}'s
+#' that were 0 will be zero-ed out in \code{fmri} (i.e., the partition of
+#' that voxel will be set to 0). If no elements of any partition originally 
+#' in \code{fmri} exist anymore after this procedure, its corresponding 
+#' column in \code{fmri} will be removed. On the flip side, if there were
+#' voxel in \code{parcellation} that were non-zero, but its corresponding
+#' voxel in \code{fmri} is empty, the voxel will be assigned an entirely
+#' new parcellation in the output of \code{applyMask}, and its corresponding
+#' column in \code{fmri$data2d} will be the column of all 0's. 
+#' 
+#' Here, \code{fmri} and \code{parcellation} must have the same size. 
+#' 
+#' We call this procedure applying a mask because this procedure does not
+#' respect the partitioning in \code{parcellation}. It only takes into account
+#' which voxels are zero and non-zero in \code{parcellation}. 
+#'
+#' @param fmri the \code{BrcFmri} instance
+#' @param parcellation the \code{BrcParcellation} instance of the same 
+#' dimension as \code{fmri}
+#'
+#' @return a new \code{BrcFmri} instance
+#' @export
 applyMask <- function(fmri, parcellation){
   if(class(fmri) != "BrcFmri") stop("fmri must be of class BrcFmri")
   if(class(parcellation) != "BrcParcellation") stop(paste("parcellation must",
